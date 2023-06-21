905e830e6bf12b170d160a9172eff0a64b44d5ea
Vahan <vahan0muradyan@gmail.com>
2023-06-21 19:57:02 +0400

    port

--- chat-server/src/main.ts
+++ chat-server/src/main.ts
@@ -8,6 +8,6 @@ async function bootstrap() {
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.useWebSocketAdapter(new IoAdapter(app));
-  await app.listen(process.env.PORT || 5000);
+  await app.listen(process.env.PORT || 3003);
 }
 bootstrap();
