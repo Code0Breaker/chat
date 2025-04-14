c93291b86b3922f2281d221830c7f1195803df37
Code0Breaker <80605026+Code0Breaker@users.noreply.github.com>
2025-04-14 22:47:49 +0400

    fix

--- chat-app/src/apis/baseUrl.ts
+++ chat-app/src/apis/baseUrl.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const url = import.meta.env.VITE_APP_DEV_URL
+export const url = import.meta.env.VITE_APP_SERVER_URL
 // VITE_APP_DEV_URL
 // VITE_APP_SERVER_URL
 export const base_url = axios.create({
