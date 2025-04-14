116c7266dceba926a21d8f854e4424cb37546063
Code0Breaker <80605026+Code0Breaker@users.noreply.github.com>
2025-04-14 22:52:50 +0400

    fix:auth

--- chat-server/src/main.ts
+++ chat-server/src/main.ts
@@ -6,7 +6,12 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: 'https://chat.animehub.club', // <-- frontend URL
+    credentials: true, // <-- required to send cookies or Authorization headers
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
   app.useGlobalPipes(new ValidationPipe());
   app.useWebSocketAdapter(new IoAdapter(app));
   app.use(cookieParser());
