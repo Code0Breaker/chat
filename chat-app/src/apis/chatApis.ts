c9bed87e1511f674f72f41ccfc7e166edc75f818
code0breaker <vahan0muradyan@gmail.com>
2025-07-24 19:44:59 +0400

    fix

--- chat-app/src/apis/chatApis.ts
+++ chat-app/src/apis/chatApis.ts
@@ -1,42 +1,43 @@
 import { IChat, IMessage } from "../types"
-import { authrised_url } from "./baseUrl"
+import { base_url } from "./baseUrl"
+
 
 export const getContacts = async (): Promise<IChat[]> => {
-    const { data } = await authrised_url.get('chat/getAllRooms' )
+    const { data } = await base_url.get('chat/getAllRooms' )
     return data
 }
 
 export const getChat = async (roomId:string): Promise<IChat> => {
-    const { data } = await authrised_url.get(`chat/getChat/${roomId}`)
+    const { data } = await base_url.get(`chat/getChat/${roomId}`)
     return data
 }
 
 export const selectChat = async (selectedRoom: string): Promise<IChat> => {
-    const { data } = await authrised_url.get('chat/getMessages/' + selectedRoom)
+    const { data } = await base_url.get('chat/getMessages/' + selectedRoom)
     return data
 }
 
 export const sendMessage = async (id: string, content: string) => {
-    const { data } = await authrised_url.post('messages', { chatId: id, content, myId: localStorage._id })
+    const { data } = await base_url.post('messages', { chatId: id, content, myId: localStorage._id })
     return data
 }
 
 export const getUnreadMessages = async():Promise<IMessage[]>=>{
-    const { data } = await authrised_url.get(`messages/${localStorage._id}`)
+    const { data } = await base_url.get(`messages/${localStorage._id}`)
     return data
 }
 
 export const chancgeUnwatchStatus = async(ids:string[]) =>{
-    const { data } = await authrised_url.patch(`messages`, {ids})
+    const { data } = await base_url.patch(`messages`, {ids})
     return data
 }
 
 export const createRoom = async(selectedId:string)=>{
-    const {data} = await authrised_url.post('/chat/create-room',{_id: [selectedId], myId:localStorage._id})
+    const {data} = await base_url.post('/chat/create-room',{_id: [selectedId], myId:localStorage._id})
     return data
 }
 
 export const chatSearch = async(text:string)=>{
-    const {data} = await authrised_url.post('/chat/search',{text})
+    const {data} = await base_url.post('/chat/search',{text})
     return data
 }
